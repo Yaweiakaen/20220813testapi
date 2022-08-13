ce7c04fe39f75441d5e7f1829fbc91e5ec39bb61
student <student@DESKTOP-4ARRLB2>
2022-08-13 12:33:33 +0800

    1233updata for jsonarray

--- mv0813/src/main/java/model/StudentDAO.java
+++ mv0813/src/main/java/model/StudentDAO.java
@@ -1,25 +1,28 @@
 package model;
+
 import java.io.PrintWriter;
 import java.sql.*;
 
 public class StudentDAO {
-      public boolean addArray(Student[ ]  arry ) {
-    	  Connection conn=null;	     
-	        try {
-	            Class.forName("com.mysql.cj.jdbc.Driver");
-	            conn =  DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels","root","1234");
-	            PreparedStatement  st=conn.prepareStatement("insert into classicmodels.student(username,age)values(?,?)");
-	            st.setString(1,arry[0].getName());
-	            st.setInt(2, arry[0].getAge());
-	            int r=st.executeUpdate();
-	            conn.close();
-	            if(r>0)
-	            	return true;
-	        }
-	        catch(Exception ex)
-	        {
-	            System.out.println(ex.getMessage());
-	        }
-	        return false;
-      }
+	public boolean addArray(Student[] arry) {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "root", "1234");
+			for (Student s : arry) {
+				PreparedStatement st = conn
+						.prepareStatement("insert into classicmodels.student(username,age)values(?,?)");
+				st.setString(1, s.getName());
+				st.setInt(2, s.getAge());
+				int r = st.executeUpdate();
+				if(r==0)
+					return false;
+			}
+			conn.close();
+			return true;
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		return false;
+	}
 }
