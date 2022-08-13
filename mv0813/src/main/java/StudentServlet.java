e4d2b239b611eec8ca6f22885dd4177411261091
student <student@DESKTOP-4ARRLB2>
2022-08-13 11:06:15 +0800

    20220813demo

---
+++ mv0813/src/main/java/StudentServlet.java
@@ -0,0 +1,51 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Student;
+import com.google.gson.*;
+/**
+ * Servlet implementation class StudentServlet
+ */
+@WebServlet("/StudentServlet")
+public class StudentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+  		//String jsonString = "{\"name\":\"Maxsu\", \"age\":24}"; 
+		String jsonString=request.getParameter("stuData");
+		//System.out.println(jsonString);
+	      GsonBuilder builder = new GsonBuilder(); 
+	     // builder.setPrettyPrinting();
+
+	      Gson gson = builder.create(); 
+	      Student student = gson.fromJson(jsonString, Student.class); 		
+		  response.getWriter().append(student.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
